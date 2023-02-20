99445da87437e4c545ded5e4f2ad6f9b43c87eeb
jmf5pu <59934098+jmf5pu@users.noreply.github.com>
2023-02-19 20:20:48 -0500

    add async function to button click

--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -1,7 +1,13 @@
+const path = require('path');
+
+async function sleep_and_add(ms, num1, num2) {
+   await new Promise(resolve => setTimeout(resolve, ms));
+   return num1 + num2
+} 
+
 let $ = require('jquery');
-let count = 0;
-$('#click-counter').text(count.toString());
-$('#countbtn').on('click', () => {
-   count ++ 
-   $('#click-counter').text(count)
+$('#click-counter').text("result goes here");
+$('#countbtn').on('click', async () => {
+   result = await sleep_and_add(1000, 3, 5)
+   $('#click-counter').text(result)
 });
